d637078f644aa0877326abb8f7a87bbc39ccf6bf
dlancer <dmdpost@gmail.com>
2016-04-03 14:33:47 +0500

    simple json response mixin added

--- appcore/views/mixins.py
+++ appcore/views/mixins.py
@@ -1,6 +1,7 @@
 """Implements decorator chaining mixin for class based view"""
 
 from django.contrib import messages
+from django.http import JsonResponse
 
 
 class DecoratorChainingMixin(object):
@@ -14,7 +15,9 @@ class DecoratorChainingMixin(object):
 
 
 class FormMessageMixin(object):
-    """Make it easy to display notification messages when using Class Based Views"""
+    """
+    Make it easy to display notification messages when using Class Based Views.
+    """
 
     def delete(self, request, *args, **kwargs):
         message = None
@@ -37,3 +40,26 @@ class FormMessageMixin(object):
         if message is not None:
             messages.error(self.request, message)
         return super(FormMessageMixin, self).form_invalid(form)
+
+
+class JSONResponseMixin(object):
+    """
+    A mixin that can be used to render a JSON response.
+    """
+    def render_to_json_response(self, context, **response_kwargs):
+        """
+        Returns a JSON response, transforming 'context' to make the payload.
+        """
+        return JsonResponse(
+            self.get_data(context),
+            **response_kwargs
+        )
+
+    def get_data(self, context):
+        """
+        Returns an object that will be serialized as JSON by json.dumps().
+        """
+        # Note: In reality, you'll need to do much more complex handling to ensure that arbitrary
+        # objects -- such as Django model instances or querysets -- can be serialized as JSON.
+        # You should implement this in your app.
+        return context
